fcd5e81d6e09a855755ae72e6c0fd8c8a4fb1a3d
ChristianMartinezTech <crismartine.e@gmail.com>
2022-01-13 20:31:34 -0500

    Coded 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,26 +8,27 @@ sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
 # Create the folder /data/ if it doesn’t already exist
-mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 
 # Create HTML file
+sudo touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Symbolic link /data/web_static/current linked to /data/web_static/releases/test/
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/
 # to hbnb_static
-sudo sed -i "43i location /nombre_de_slug {todo lo que va adentro}"
+sudo sed -i '25i location /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart nginx
 sudo service nginx restart
