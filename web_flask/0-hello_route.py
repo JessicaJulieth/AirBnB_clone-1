c7824d0cc2df1f12a111384f218c08ab29acc74b
JessicaJulieth <3497@holbertonschool.com>
2022-02-02 22:52:23 -0500

    Starts a flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/'strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def Hello_HBNB():
     """
     Returns: Hello HBNB!
@@ -16,4 +16,4 @@ def Hello_HBNB():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
